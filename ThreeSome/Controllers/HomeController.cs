f626f0546e82b7ce912579dbaac655a1a4c3f1b9
Kha <phalekham@gmail.com>
2023-09-21 00:26:12 +0700

    add something

--- ThreeSome/Controllers/HomeController.cs
+++ ThreeSome/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace ThreeSome.Controllers
             return View();
         }
 
+        public ActionResult Trending()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
